92adb09602d0ded3bfe6b2c8ce1eb815410d6a5d
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-11-23 06:07:32 +0200

    save changes

--- models/expense.js
+++ models/expense.js
@@ -15,10 +15,11 @@ const expenseSchema = Schema({
   },
   category: {
     type: String,
-    required: [true, 'Select category'],
+    default: '',
   },
   description: {
     type: String,
+    default: '',
   },
   sum: {
     type: Number,
@@ -37,7 +38,7 @@ const expenseSchema = Schema({
 const joiSchema = Joi.object({
   date: Joi.string(),
   type: Joi.string().required(),
-  category: Joi.string().required(),
+  category: Joi.string(),
   description: Joi.string(),
   sum: Joi.number().positive(),
   amount: Joi.number(),
